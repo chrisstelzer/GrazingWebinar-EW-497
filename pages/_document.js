fa9545c5b3c5ac5615cf4f3d060b44c12d93fa83
Chris Stelzer <34406273+chrisstelzer@users.noreply.github.com>
2025-06-16 17:39:42 -0600

    GTM attempt #2

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,29 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-         <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.server.agriculturalinsights.com/5imesvfns.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','cywxoi=aWQ9R1RNLVRHWFhWVFg%3D&page=1');</script>
-<!-- End Google Tag Manager -->
+          {/* Google Tag Manager (via Stape custom loader) */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s);j.async=true;j.src=
+                'https://load.server.agriculturalinsights.com/5imesvfns.js?'+i;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','cywxoi=aWQ9R1RNLVRHWFhWVFg%3D&page=1');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
         </Head>
         <body>
-          <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://load.server.agriculturalinsights.com/ns.html?id=GTM-TGXXVTX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://load.server.agriculturalinsights.com/ns.html?id=GTM-TGXXVTX"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
